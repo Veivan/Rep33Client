a5842f4ab09a28ea8b5026b61cb82716e79792a8
kolifirenko@moscow-cargo.com <kolifirenko@moscow-cargo.com>
2021-10-22 11:26:52 +0300

    PAR-312 Изменение Report33.Добавил file-saver, управление параметром emails. Поменял Layout.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Form, Button, Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
 import Calendar from 'react-calendar'
+import {saveAs} from 'file-saver';
 
 export default class App extends Component {
 
 constructor(props) {
     super(props);
-	  this.state = {
-		use_timer: "false",
-		launch_time: "",
-		save2bd: "false",
-		launch_date : new Date(),
-		rep_save2bd: "false",
-		rep_usesaved: "false",
-		rep_getfile: "false"
-	  }; 
+		let date = new Date(); 
+		date.setDate(date.getDate()-1);
+	  	this.state = {
+			use_timer: "false",
+			launch_time: "",
+			save2bd: "false",
+			emails: "",
+			launch_date : date,
+			rep_save2bd: "false",
+			rep_usesaved: "false",
+			rep_getfile: "false"		
+	  	}; 
 	  
-	  this.config = require('./confdev.json');
-	  //this.config = require('./confprod.json');
+	  	this.config = require('./confdev.json');
+	  	//this.config = require('./confprod.json');
 	}
 
 	handleChange = (event) => {
@@ -80,12 +84,19 @@ constructor(props) {
 		axios.get(this.config.reportrouteadmin + "?" + params.toString())
 		.then((response) => {
 			console.log(response);
-		  }, (error) => {
-			console.log(error);
-		  });
+		})
+		.then(function (response) {
+			return response.blob();
+		})
+		.then(function(blob) {
+			saveAs(blob, "yourFilename.xlsx");
+		})
+		.catch(error => {
+			//whatever
+		});
 //		document.location.reload();
 	}
-
+	
 	componentDidMount() {
 		axios.get(this.config.paramsroute)		
 			.then((response) => {
@@ -93,6 +104,7 @@ constructor(props) {
 				this.setState({use_timer : response.data.use_timer});
 				this.setState({launch_time : response.data.launch_time});
 				this.setState({save2bd : response.data.save2bd});
+				this.setState({emails : response.data.emails});
 			})
 			.catch(function (error) {
 				console.log(error);
@@ -109,10 +121,14 @@ constructor(props) {
 			<Form>
 				<Row>
 					<h1>Настройки автоматического запуска</h1>
-					<Form.Control plaintext readOnly value="Формирование отчёта автоматически по таймеру на 'вчера'." />
-					<Form.Control plaintext readOnly value="Отчёт сохраняется в БД. Сохранённые данные не используются." />
-					<Form.Control plaintext readOnly value="Отправка отчёта списку адресатов." />
+					<Form.Text muted>
+						Формирование отчёта автоматически по таймеру на 'вчера'.<br />
+						Сохранённые данные не используются.<br />
+						Отчёт сохраняется в БД.<br />
+						Отправка отчёта списку адресатов.
+					</Form.Text>
 				</Row>
+				<br />
 				<Row>
 					<Col>
 						<Form.Check name='use_timer' type="switch" id="use_timerId" 
@@ -132,43 +148,61 @@ constructor(props) {
 							label="Сохранять в БД" checked={this.state.save2bd === "true"}
 							onChange={this.handleCheckClick} />
 					</Col>
+ 				</Row>
+				<Row>
+					<Col>
+						<InputGroup className="mb-3">
+							<InputGroup.Text >Список адресатов</InputGroup.Text>
+    						<FormControl name='emails' placeholder="email1, email2..."  
+								value={this.state.emails}
+								onChange={this.handleChange} />
+  						</InputGroup>
+    				</Col>
+
+				</Row>
+				<Row>
 					<Col>
 						<Button className='my-2' variant="primary" onClick={this.submit}>Сохранить</Button> 
 					</Col>
- 				</Row>
+				</Row>
+
 				<br /><br />
 
 				<Row>
 					<h1>Настройки ручного запуска</h1>
 					<Col xs={3}> 
 						<Form.Label >Дата отчёта</Form.Label>
-						<Form.Control plaintext readOnly value={this.state.launch_date} />
+						<Form.Control plaintext readOnly value={this.state.launch_date.toLocaleDateString("en-US")} />
 					    <Calendar name='launch_date'
     					    onChange={this.handleCalend}
         					value={this.state.launch_date}
       					/>
 					</Col>
 					<Col> 
-						<Form.Control plaintext readOnly value="Формирование отчёта на выбранную дату." />
-						<Form.Control plaintext readOnly value="Используются сохранённые данные." />
-						<Form.Control plaintext readOnly value="Отчёт в БД не сохраняется." />
-						<Form.Control plaintext readOnly value="Отправка отчёта списку адресатов." />
-						<br /><br />
+						<Form.Text muted>
+							Формирование отчёта на выбранную дату.<br />
+							Используются сохранённые данные.<br />
+							Отчёт в БД не сохраняется.<br />
+							Отправка отчёта списку адресатов.
+						</Form.Text>
+						<br />
 						<Button className='my-2' variant="primary" onClick={this.buildsend}>Построить и отправить</Button> 
 					</Col>
-					<Col> 
-						<Form.Control plaintext readOnly value="Формирование отчёта на выбранную дату." />
-						<Form.Control plaintext readOnly value="Использовать сохранённые данные - из настроек." />
-						<Form.Control plaintext readOnly value="Сохранять отчёт в БД - из настроек." />
-						<Form.Control plaintext readOnly value="Сохранить файл отчёта локально - из настроек." />
-						<br /><br />
-						<Form.Check className="d-flex" name='rep_usesaved' type="switch" id="rep_usesavedId" 
+					<Col>  
+						<Form.Text muted>
+							Формирование отчёта на выбранную дату.<br />
+							Использовать сохранённые данные - из настроек.<br />
+							Сохранять отчёт в БД - из настроек.<br />
+							Сохранить файл отчёта локально - из настроек.(under construction)
+						</Form.Text>
+						<br />
+						<Form.Check name='rep_usesaved' type="switch" id="rep_usesavedId" 
 							label="Использовать сохранённые данные" checked={this.state.rep_usesaved === "true"}
 							onChange={this.handleCheckClick} />
-						<Form.Check className="d-flex" name='rep_save2bd' type="switch" id="rep_save2bdId" 
+						<Form.Check name='rep_save2bd' type="switch" id="rep_save2bdId" 
 							label="Сохранить в БД" checked={this.state.rep_save2bd === "true"}
 							onChange={this.handleCheckClick} />
-						<Form.Check className="d-flex" name='rep_getfile' type="switch" id="rep_getfiled" 
+						<Form.Check name='rep_getfile' type="switch" id="rep_getfiled" disabled
 							label="Сохранить файл отчёта локально" checked={this.state.rep_getfile === "true"}
 							onChange={this.handleCheckClick} />
 						<Button className='my-2' variant="primary" onClick={this.buildadmin}>Построить</Button> 
